864d38003c2c75032564b5f27065a084b6c97194
rouha <rouha@DESKTOP-MU91PTE>
2019-09-10 21:07:59 -0400

    Add project files.

---
+++ inclass assignment 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace inclass_assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your years of experience");
+            try
+            {
+                string input = Console.ReadLine();
+                int value_of_input = int.Parse(input);
+                for (int i = 0; i < value_of_input; i++)
+                {
+                    Console.WriteLine("Your experience is " + i.ToString());
+
+                }
+                Console.WriteLine("Press any key to exit the program ...");
+                Console.ReadKey(true);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter years of expereicne in integer");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
